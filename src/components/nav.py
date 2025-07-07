670ab864de59ba59d9992f0297b71f6381679003
Joel Leandro Nardi <jnardi@sweatworks.net>
2025-07-07 23:09:32 +0200

    Progress on nav-bar

--- src/components/nav.py
+++ src/components/nav.py
@@ -16,16 +16,39 @@ def get_appbar(title: str, page: ft.Page, on_back=None, toggle_theme=None) -> ft
             spacing=10,
             vertical_alignment=ft.CrossAxisAlignment.CENTER
         ),
-        title=ft.TextButton(
-                  text="Joel Leandro Nardi", 
-                  on_click=lambda e: page.go(f"/"), 
-                  style=ft.ButtonStyle(
-                      text_style=ft.TextStyle(
-                          size=18,
-                          weight=ft.FontWeight.BOLD,
-                          color=ft.Colors.ON_SECONDARY_CONTAINER
-                      )
-                  )),
+        title=ft.Row(  # Wrap the title button in a Row to make it expand
+            controls=[
+                ft.TextButton(
+                    on_click=lambda e: page.go("/"),
+                    content=ft.Row(
+                        controls=[
+                            ft.Icon(
+                                name=ft.Icons.DATA_ARRAY_ROUNDED,
+                                size=36,
+                                color=ft.Colors.ON_TERTIARY_CONTAINER
+                            ),
+                            ft.Text(
+                                "Joel Leandro Nardi ",
+                                size=24,
+                                weight=ft.FontWeight.BOLD,
+                                color=ft.Colors.ON_TERTIARY_CONTAINER,
+                            ),
+                        ],
+                        spacing=8,
+                        alignment=ft.MainAxisAlignment.START,
+                        vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                        expand=True  # Make text+icon row expand
+                    ),
+                    style=ft.ButtonStyle(
+                        padding=0,
+                        shape=ft.RoundedRectangleBorder(radius=0),
+                    )
+                )
+            ],
+            expand=True,  # Make title row fill horizontal space
+            alignment=ft.MainAxisAlignment.START,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER
+        ),
         center_title=False,
         bgcolor=ft.Colors.SECONDARY_CONTAINER,
         actions=[
